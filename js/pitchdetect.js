8042485593b27a6ad6363833027532e32c5088f1
Chris Wilson <cwilso@gmail.com>
2012-07-20 16:59:07 -0700

    Fully functional.

--- js/pitchdetect.js
+++ js/pitchdetect.js
@@ -3,10 +3,16 @@ var isPlaying = false;
 var sourceNode = null;
 var analyser = null;
 var theBuffer = null;
+var detectorElem, 
+	canvasElem,
+	pitchElem,
+	noteElem,
+	detuneElem,
+	detuneAmount;
 
 window.onload = function() {
 	var request = new XMLHttpRequest();
-	request.open("GET", "../sounds/whistling.ogg", true);
+	request.open("GET", "../sounds/whistling2.ogg", true);
 	request.responseType = "arraybuffer";
 	request.onload = function() {
 	  audioContext.decodeAudioData( request.response, function(buffer) { 
@@ -14,6 +20,37 @@ window.onload = function() {
 		} );
 	}
 	request.send();
+
+	detectorElem = document.getElementById( "detector" );
+	canvasElem = document.getElementById( "output" );
+	pitchElem = document.getElementById( "pitch" );
+	noteElem = document.getElementById( "note" );
+	detuneElem = document.getElementById( "detune" );
+	detuneAmount = document.getElementById( "detune_amt" );
+
+	detectorElem.ondragenter = function () { 
+		this.classList.add("droptarget"); 
+		return false; };
+	detectorElem.ondragleave = function () { this.classList.remove("droptarget"); return false; };
+	detectorElem.ondrop = function (e) {
+  		this.classList.remove("droptarget");
+  		e.preventDefault();
+		theBuffer = null;
+
+	  	var reader = new FileReader();
+	  	reader.onload = function (event) {
+	  		audioContext.decodeAudioData( event.target.result, function(buffer) {
+	    		theBuffer = buffer;
+	  		}, function(){alert("error loading!");} ); 
+
+	  	};
+	  	reader.onerror = function (event) {
+	  		alert("Error: " + reader.error );
+		};
+	  	reader.readAsArrayBuffer(e.dataTransfer.files[0]);
+	  	return false;
+	};	
+
 }
 
 function togglePlayback() {
@@ -25,7 +62,8 @@ function togglePlayback() {
         sourceNode = null;
         analyser = null;
         isPlaying = false;
-        return "play";
+        webkitCancelAnimationFrame( rafID );
+        return "start";
     }
 
     sourceNode = audioContext.createBufferSource();
@@ -38,6 +76,7 @@ function togglePlayback() {
     analyser.connect( audioContext.destination );
     sourceNode.noteOn( now );
     isPlaying = true;
+    updatePitch();
 
     return "stop";
 }
@@ -49,7 +88,7 @@ var buf = new Uint8Array( buflen );
 var MINVAL = 134;  // 128 == zero.  MINVAL is the "minimum detected signal" level.
 
 function findNextPositiveZeroCrossing( start ) {
-	var i = start;
+	var i = Math.ceil( start );
 	var last_zero = -1;
 	// advance until we're zero or negative
 	while (i<buflen && (buf[i] > 128 ) )
@@ -71,18 +110,32 @@ function findNextPositiveZeroCrossing( start ) {
 	if (last_zero == -1)
 		last_zero = i;
 
-	if (i==buflen)
+	if (i==buflen)	// We didn't find any more positive zero crossings
 		return -1;
-	else
-		return last_zero;
+
+	// The first sample might be a zero.  If so, return it.
+	if (last_zero == 0)
+		return 0;
+
+	// Otherwise, the zero might be between two values, so we need to scale it.
+
+	var t = ( 128 - buf[last_zero-1] ) / (buf[last_zero] - buf[last_zero-1]);
+	return last_zero+t;
 }
 
 var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
 function noteFromPitch( frequency ) {
 	var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
-	var midiNote = Math.round( noteNum ) + 57;
-	return  (noteStrings[midiNote%12] + " - bend " + (noteNum + 57 - midiNote)/midiNote );
+	return Math.round( noteNum ) + 69;
+}
+
+function frequencyFromNoteNumber( note ) {
+	return 440 * Math.pow(2,(note-69)/12);
+}
+
+function centsOffFromPitch( frequency, note ) {
+	return Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
 }
 
 function updatePitch( time ) {
@@ -116,15 +169,43 @@ function updatePitch( time ) {
 	}
 
 // confidence = num_cycles / num_possible_cycles = num_cycles / (audioContext.sampleRate/)
+	var confidence = (num_cycles ? ((num_cycles/(pitch * buflen / audioContext.sampleRate)) * 100) : 0);
+
+/*
 	console.log( 
 		"Cycles: " + num_cycles + 
 		" - average length: " + sum + 
 		" - pitch: " + pitch + "Hz " +
 		" - note: " + noteFromPitch( pitch ) +
-		" - confidence: " + (num_cycles ? ((num_cycles/(pitch * buflen / audioContext.sampleRate)) * 100) : 0) + "% "
+		" - confidence: " + confidence + "% "
 		);
+*/
+	// possible other approach to confidence: sort the array, take the median; go through the array and compute the average deviation
 
-	// 2?: sort the array, take the median; go through the array and compute the average deviation
+ 	detectorElem.className = (confidence>50)?"confident":"vague";
+	// TODO: Paint confidence meter on canvasElem here.
 
-//	rafID = window.webkitRequestAnimationFrame( updatePitch );
+ 	if (num_cycles == 0) {
+	 	pitchElem.innerText = "--";
+		noteElem.innerText = "-";
+		detuneElem.className = "";
+		detuneAmount.innerText = "--";
+ 	} else {
+	 	pitchElem.innerText = Math.floor( pitch );
+	 	var note =  noteFromPitch( pitch );
+		noteElem.innerText = noteStrings[note%12];
+		var detune = centsOffFromPitch( pitch, note );
+		if (detune == 0 ) {
+			detuneElem.className = "";
+			detuneAmount.innerText = "--";
+		} else {
+			if (detune < 0)
+				detuneElem.className = "flat";
+			else
+				detuneElem.className = "sharp";
+			detuneAmount.innerText = Math.abs( detune );
+		}
+	}
+
+	rafID = window.webkitRequestAnimationFrame( updatePitch );
 }
